d8b0b2afe9695a527a68b66261008055239d2535
Nailson <nailsonlima131@gmail.com>
2022-09-27 20:58:00 -0300

    buscador de CEP

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './styles.css'
+import {FiSearch} from 'react-icons/fi'
+
 
 function App() {
+
+  const [input, setInput] = useState('')
+
+  function handleSearch(){
+    alert(input)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <h1 className="title">Buscador de CEP</h1>
+        <div className="containerInput">
+          <input
+          type="text"
+          placeholder="Digite seu cep..."
+          value={input}
+          onChange={(e)=>setInput(e.target.value)}
+          />
+
+          <button className='buttonSearch' onClick={handleSearch}>
+            <FiSearch size={25} color="#fff"/>
+          </button>
+        </div>
+        <main className='main'>
+          <h2>CEP: 5618515-450</h2>
+
+          <span>Rua: Teste testada testezinho</span>
+          <span>Complemento: perto de algum lugar</span>
+          <span>Vila Rosa</span>
+          <span>Baturité - CE</span>
+        </main>
     </div>
   );
 }
